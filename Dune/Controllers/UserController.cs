9735c10811b885e36ca3b34a4123944753ed211a
DarrenDDaley <darren.daley@hotmail.co.uk>
2018-05-15 23:03:35 +0100

    Started work on the registration functionality

---
+++ Dune/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dune.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dune.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public IActionResult Post([FromBody]UserApiModel user)
+        {
+            if (ModelState.IsValid) {
+                return Ok();
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ Dune/Models/UserApiModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dune.Models
+{
+    public class UserApiModel
+    {
+        [Required(ErrorMessage = "A username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "A email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A password is required")]
+        public string Password { get; set; }
+    }
+}
